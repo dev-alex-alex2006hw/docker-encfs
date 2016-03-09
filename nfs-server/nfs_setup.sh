93a662147a353f5ce14ab74d29ca5fe854ca75d0
ruilinchu <ruilinchu@gmail.com>
2016-03-08 16:17:24 -0800

    add

--- nfs-server/nfs_setup.sh
+++ nfs-server/nfs_setup.sh
@@ -8,6 +8,7 @@ ipaddr=$(/sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{p
 
 mkdir -p /mnt/encrypted
 mkdir -p /data
+rpcbind
 mount -t nfs -o proto=tcp,port=2049 10.0.15.11:/home/public /mnt/encrypted
 echo $enpass | encfs /mnt/encrypted/$user /data -S
 
