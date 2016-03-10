553ce57cb6933cb0ab5d62a4cf8470b82d3f5a65
ruilinchu <ruilinchu@gmail.com>
2016-03-09 17:20:15 -0800

    add

--- force_into_container.sh
+++ force_into_container.sh
@@ -6,11 +6,12 @@ user_passwd=vagrant
 RUNNING1=$(docker inspect --format="{{ .State.Running }}" shadow-$user 2> /dev/null)
 
 if [ $? -eq 1 ]; then
-    docker run -d -P --cap-add SYS_ADMIN --device /dev/fuse --name shadow-$user nfs-server $user $user_passwd `id -u $user` `id -g $user` &> /dev/null
+    docker run -d --hostname $HOSTNAME -P --cap-add SYS_ADMIN --device /dev/fuse --name shadow-$user nfs-server $user $user_passwd `id -u $user` `id -g $user` &> /dev/null
 fi
 
 if [ "$RUNNING1" == "false" ]; then
     docker restart shadow-$user
+    #recheck, if still not running, rm and start new
 fi
 
 RUNNING2=$(docker inspect --format="{{ .State.Running }}" compute-$user 2> /dev/null)
@@ -21,8 +22,8 @@ if [ $? -eq 1 ]; then
     
  #   cp /etc/passwd  /home/$user/passwd
  #   cp /etc/group  /home/$user/group
-    
-    docker run -d --net=container:shadow-$user --name compute-$user -h $HOSTNAME \
+    # --hostname confilt --net, set it in shadow
+    docker run -d --net=container:shadow-$user --name compute-$user \
 	   -v /home/$user/passwd:/etc/passwd \
 	   -v /home/$user/group:/etc/group \
 	   --cap-add SYS_ADMIN nfs-client \
@@ -31,6 +32,7 @@ fi
 
 if [ "$RUNNING2" == "false" ]; then
     docker restart compute-$user
+    #recheck, if still not running, rm and start new
 fi
 
 #    docker stop compute-$user &> /dev/null
@@ -38,6 +40,7 @@ fi
 
 ssh_port=$(docker port shadow-$user 22 | awk -F: '{print $2}')
 echo $ssh_port > /home/$user/port
-chmod 400 /home/$user/port
+chmod 600 /home/$user/port
 
+sleep 3
 ssh $user@$HOSTNAME -p $ssh_port
