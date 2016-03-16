a8990e11bbff0bcd730867f449cb6d10e4418099
ruilinchu <ruilinchu@gmail.com>
2016-03-16 11:49:30 -0700

    update

--- copy_key.sh
+++ copy_key.sh
@@ -5,4 +5,4 @@ cp /etc/ssh/ssh_host_ecdsa_key.pub nfs-client/
 cp /etc/ssh/ssh_host_ed25519_key nfs-client/
 cp /etc/ssh/ssh_host_ed25519_key.pub nfs-client/
 
-chmod 0600 ssh_host_*_key
+chmod 0600 nfs-client/ssh_host_*_key
