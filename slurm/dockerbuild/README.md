a55e1ec6b013034f5e3432bd4fb01f162eef1f34
ruilinchu <ruilinchu@gmail.com>
2016-04-11 15:48:55 -0700

    add

--- slurm/dockerbuild/README.md
+++ slurm/dockerbuild/README.md
@@ -1,17 +1,7 @@
 ```
-docker run -d --name slurmctld --net stack --hostname slurmctld -p 6817:6817 slurmctld sleep 1000000
-docker run -d --name node1 --net stack --hostname node1 slurmd sleep 1000000
+docker run -d --name slurmctld --net stack --hostname slurmctld  slurmctld slurmctld node[1-2]
+docker run -d --name node1 --net stack --hostname node1 slurmd slurmctld node[1-2]
 
-#!/bin/bash
+no need to expose port 6817
 
-# on all
-munged
-
-# on slurmctld 
-slurmctld -D -v -c &> /var/log/slurmctld.log &
-
-# on slurmd
-slurmd -D -v  &> /var/log/slurmd.log &
-
-# on all, /etc/slurm/slurm.conf must be the same
 ```
---
+++ slurm/dockerbuild/slurmctld/slurmctld_ini.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+slurmctl_host=$1
+slurmctl_addr=`ping $slurmctld_host -c 1 | grep PING | awk '{print $3}' | sed 's/\((\|)\)//g'`
+nodelist=$2
+
+# munge auth
+/usr/sbin/munged
+
+# edit slurm.conf
+#ControlMachine=
+#ControlAddr=
+sed -i "/ControlMachine=/c\ControlMachine=$slurmctl_host " /etc/slurm/slurm.conf
+sed -i "/ControlAddr=/c\ControlAddr=$slurmctl_addr " /etc/slurm/slurm.conf
+
+#NodeName=node[1-10] Procs=1 State=UNKNOWN
+#PartitionName=main Nodes=node[1-10] Default=YES MaxTime=INFINITE State=UP
+sed -i "/NodeName=/c\NodeName=$nodelist Procs=1 State=UNKNOWN" /etc/slurm/slurm.conf
+sed -i "/PartitionName=/c\PartitionName=main Nodes=$nodelist Default=YES MaxTime=INFINITE State=UP" /etc/slurm/slurm.conf
+
+# slurmctld daemon
+/usr/sbin/slurmctld -D -v -c &> /var/log/slurmctld.log &
+
+while :; do
+    sleep 900
+done
