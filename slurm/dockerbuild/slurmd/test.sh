1e28be671d5389a94413a65b4b476366f040d264
ruilinchu <ruilinchu@gmail.com>
2016-04-15 11:14:20 -0700

    add

--- slurm/dockerbuild/slurmd/test.sh
+++ slurm/dockerbuild/slurmd/test.sh
@@ -9,7 +9,7 @@ for i in `seq 21 22`; do
 	   -v /home/$user/.group:/etc/ugroup \
 	   -v /home/$user:/mnt/encry1 \
 	   --cap-add SYS_ADMIN --device /dev/fuse \
-	   compute/slurmd:encfs slurmctld2 node2[1-2]
+	   compute/slurmd slurmctld2 node2[1-2]
 
     docker exec -i node$i /usr/local/bin/setup_encfs vagrant 1000 1000
 done
