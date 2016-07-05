0c2aab1a276df494a2e474376bf7f5f12e7be56a
ruilinchu <ruilinchu@gmail.com>
2016-07-05 12:29:50 -0700

    add

--- registry/run.sh
+++ registry/run.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+#on all nodes
 cat >  /etc/docker/daemon.json << EOF
 {
     "insecure-registries": ["10.0.15.11:5000"]
@@ -5,7 +8,12 @@ cat >  /etc/docker/daemon.json << EOF
 EOF
 
 systemctl restart docker
+
+## on server side
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
+
 docker tag compute 10.0.15.11:5000/compute
 docker push 10.0.15.11:5000/compute
+
+## on clients do docker pull
 
