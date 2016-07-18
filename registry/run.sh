e153571cac0e835f21c5499faf55c6a839c684e7
ruilinchu <ruilinchu@gmail.com>
2016-07-18 15:12:33 -0700

    add

--- registry/run.sh
+++ registry/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#on all nodes
+#on all nodes, cluster stuff is for multi-node clustering
 cat >  /etc/docker/daemon.json << EOF
 {
-    "insecure-registries": ["10.0.15.16:5000"]
+    "insecure-registries": ["10.0.15.16:5000"],
+    "cluster-store": "consul://10.0.15.16:8500",
+    "cluster-advertise": "enp0s8:2375"
 }
 EOF
 
@@ -12,8 +14,11 @@ systemctl restart docker
 ## on server side
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
+## consul for mlti-node clustering, overlay network
+docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
+
 docker tag login 10.0.15.16:5000/login
-docker push 10.0.15.16:5000/compute
+docker push 10.0.15.16:5000/login
 
 ## on clients do docker pull
 
