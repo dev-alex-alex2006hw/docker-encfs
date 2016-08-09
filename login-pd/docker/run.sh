e0e8a86d5fb26bbb3a10d609d885b9baf3d30c70
ruilinchu <ruilinchu@gmail.com>
2016-08-08 17:24:53 -0700

    add

--- login-pd/docker/run.sh
+++ login-pd/docker/run.sh
@@ -5,24 +5,25 @@ user=$1
 
 enpass=$(/usr/local/bin/retrieve_pass $user)
 
-chmod 0700 /mnt/do_not_use
+mkdir /home/$user
+chown -R $user: /home/$user
 
-echo $enpass | encfs /mnt/do_not_use /home/$user -S --public
+echo $enpass | runuser -l $user -c "ENCFS6_CONFIG=/usr/local/etc/.encfs6.xml encfs -o nonempty /mnt/do_not_use /home/$user -S "
 
-rm -fr $ENCFS6_CONFIG
+#if [ ! -f /home/$user/.bashrc ]; then 
+#   cp /usr/local/src/.bashrc /home/$user/
+#   chown $user /home/$user/.bashrc  
+#fi
 
-if [ ! -f /home/$user/.bashrc ]; then 
-   cp /usr/local/src/.bashrc /home/$user/
-   chown $user /home/$user/.bashrc  
-fi
-
-if [ ! -f /home/$user/.bash_profile ]; then
-   cp /usr/local/src/.bash_profile /home/$user/
-   chown $user /home/$user/.bash_profile
-fi
+#if [ ! -f /home/$user/.bash_profile ]; then
+#   cp /usr/local/src/.bash_profile /home/$user/
+#   chown $user /home/$user/.bash_profile
+#fi
 
 /usr/sbin/sshd -D &> /dev/null &
 /usr/sbin/trqauthd &> /dev/null &
+
+rm -fr $ENCFS6_CONFIG
 
 # # exit out of idle containers to save resources and to allow opportunities to pull in the lastest container image
 while :; do
